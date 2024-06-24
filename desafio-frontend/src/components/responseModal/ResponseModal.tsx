e6d9c682822118358c1376135321fe897ab3ca15
Thiago Oss Emer Eizerik <49419328+ThiagoEize@users.noreply.github.com>
2024-06-24 07:40:45 -0300

    randle response modal styled

--- desafio-frontend/src/components/responseModal/ResponseModal.tsx
+++ desafio-frontend/src/components/responseModal/ResponseModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Toast, ToastContainer } from "react-bootstrap";
+import styles from "./ResponseModal.module.css";
 
 interface ResponseModalProps {
   title: string;
@@ -12,9 +13,18 @@ const ResponseModal: React.FC<ResponseModalProps> = ({ title, message }) => {
   const ToastHeader: any = Toast.Header;
   const ToastBody: any = Toast.Body;
 
+  // Determine the class name based on the title
+  const toastClass =
+    title === "Sucesso" ? styles.success : title === "Erro" ? styles.error : "";
+
   return (
     <ToastContainerComponent position="bottom-start">
-      <ToastComponent show={true} delay={5000} autohide>
+      <ToastComponent
+        className={`${toastClass}`}
+        show={true}
+        delay={5000}
+        autohide
+      >
         <ToastHeader>
           <strong className="me-auto">{title}</strong>
         </ToastHeader>
